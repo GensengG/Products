3c0671b4f89c07d8253938547e198a6df1cdafff
GensengG <168531024+GensengG@users.noreply.github.com>
2025-01-14 23:33:02 +0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,38 +1,75 @@
-import "../App";
-import { useState } from "react";
+import "../App.jsx";
 
 export const Button = (stateSwitch) => {
+    let products = [
+        {
+          name: "Nike Metcon 2",
+          price: "130",
+          color: "red",
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/1.jpg",
+          id: "product 1"
+        }, {
+          name: "Nike Metcon 2",
+          price: "130",
+          color: "green",
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/2.jpg",
+          id: "product 2"
+        }, {
+          name: "Nike Metcon 2",
+          price: "130",
+          color: "blue",
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/3.jpg",
+          id: "product 3"
+        }, {
+          name: "Nike Metcon 2",
+          price: "130",
+          color: "black",
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/4.jpg",
+          id: "product 4"
+        }, {
+          name: "Nike free run",
+          price: "170",
+          color: "black",
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/7.jpg",
+          id: "product 5"
+        }, {
+          name: "Nike Metcon 3",
+          price: "150",
+          color: "green",
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/5.jpg",
+          id: "product 6"
+        }
+    ]
 
-    const {data} = stateSwitch;
-    const dataValue = data.buttonValue;
-    const dataProducts = data.products;
+    let {btnTextContent} = stateSwitch;
 
     let result = [];
 
-    if(dataValue === "Переключаем на список"){
-        result = dataProducts.map((item) => {
-            <div key={item.id} className="list__item">
-              <img src={item.img} className="list__image" /> 
-              <div className="list__name">{item.name}</div> 
-              <div className="list__color">{item.color}</div> 
-              <div className="list__price">{item.price}</div> 
-            </div>
-        })
+    if(btnTextContent === "Переключаем на карточки"){
+        result = products.map(item => (
+          <div className="list__item">
+            <img src={item.img} className="list__image" /> 
+            <h2 className="list__name">{item.name}</h2> 
+            <div className="list__color">{item.color}</div> 
+            <div className="list__price">${item.price}</div> 
+            <button className="list__btn">{"ADD TO CART"}</button>
+          </div>
+        ))
     } else {
-        result = dataProducts.map((item) => {
-            <div key={item.id} className="cards__item">
-              <img src={item.img} className="cards__image" /> 
-              <div className="cards__name">{item.name}</div> 
-              <div className="cards__color">{item.color}</div> 
-              <div className="cards__price">{item.price}</div> 
-            </div>
-          })
+        result = products.map(item => (
+          <div className="cards__item">
+            <h2 className="cards__name">{item.name}</h2> 
+            <div className="cards__color">{item.color}</div> 
+            <img src={item.img} className="cards__image" /> 
+            <div className="cards__price">${item.price}</div> 
+            <button className="cards__btn">{"ADD TO CART"}</button>
+          </div>
+        ))
     }
 
     return (
-        <div className="button">
-            {result}
-        </div>
+      <div className="container">
+        {result}
+      </div>
     );
-
 }
